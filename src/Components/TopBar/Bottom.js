626372ea47eb57fd039ec7859dd2a45b9d458b43
greg <gsc229@gmail.com>
2021-06-30 02:56:11 -0400

    topbar calling it good for now

---
+++ src/Components/TopBar/Bottom.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Bottom = () => {
+  return (
+    <div className="top-bar-bottom">
+      <div className="spaces-and-number">
+        <div className="spaces">
+            Our Spaces
+        </div>
+        <div className="number">
+            +1 (718) 786-8400
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Bottom
--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Grid from '@material-ui/core/Grid'
+import Bottom from './Bottom'
 import ItemsLarge from './ItemsLarge'
 import ItemsSmall from './ItemsSmall'
 import logo from '../../images/one-stop-logo (1).png'
@@ -12,15 +13,21 @@ const listItems = ["Home", "About Us", "Our Team", "Our Clients", "Message For D
 const useStyles = makeStyles((theme) => {
   return{
     topBar: {
+      backgroundColor: 'transparent'
+    },
+    appBarTop: {
       backgroundColor: 'rgba(0,0,0,.9)',
       display: 'flex',
       justifyContent: 'center',
-      height: '150px',
-      position: 'fixed'
+      height: '150px'
+    },
+    gridContainer: {
+      width: '90%',
+      margin: 'auto'
     },
     logo: {
       width: '170px',
-      border: '1px solid green'
+      cursor: 'pointer'
     },
     contactBtn: {
       backgroundColor: 'white'
@@ -37,22 +44,26 @@ const TopBar = () => {
     <AppBar
     elevation={0} 
     className={classes.topBar}>
-      <Grid
-      alignItems='center'
-      justify='space-between'
-      container 
-      spacing={2}>
-        <Grid style={{border: '1px solid hotpink'}} item xs={3} >
-          <img 
-          className={classes.logo}
-          src={logo} alt="Logo" />
+      <div
+      className={classes.appBarTop}>
+        <Grid
+        className={classes.gridContainer}
+        alignItems='center'
+        justify='space-between'
+        container>
+          <Grid item xs={3} >
+            <img 
+            className={classes.logo}
+            src={logo} alt="Logo" />
+          </Grid>
+          <Grid  item xs={9} >
+            {!isSmall && <ItemsLarge listItems={listItems} />}
+            {isSmall &&  <ItemsSmall listItems={listItems} />
+            }
+          </Grid>
         </Grid>
-        <Grid style={{border: '1px solid hotpink'}}  item xs={9} >
-          {!isSmall && <ItemsLarge listItems={listItems} />}
-          {isSmall &&  <ItemsSmall listItems={listItems} />
-          }
-        </Grid>
-      </Grid>
+      </div>
+      <Bottom />
     </AppBar>
   )
 }
